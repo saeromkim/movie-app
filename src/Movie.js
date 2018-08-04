098b77c0091b0a7741b5ced86da0be660ffe1d7d
Rainist <ksr24777@naver.com>
2018-08-04 21:25:23 +0900

    first commit

---
+++ src/Movie.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Movie.css';
+
+
+function Movie({title, poster}){
+    return(
+        <div>
+                <MoviePoster poster={poster}/>
+                <h1>{title}</h1>
+            </div> 
+    )
+}
+
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster" />
+    )
+}
+
+Movie.propTypes={
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
+}
+
+MoviePoster.propTypes={
+    poster : propTypes.string.isRequired
+}
+
+
+export default Movie;
